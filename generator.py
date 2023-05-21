898e117a0d91a326f6541632d423fb64f0af8152
Abby Newcomb <abby@Abbys-MacBook-Pro-2.local>
2023-05-21 16:47:16 -0500

    bug fixes and added temperature support

--- generator.py
+++ generator.py
@@ -3,17 +3,17 @@
 import os
 import time
 import sys
-# import magenta
-# import note_seq
-# import tensorflow
-# from magenta.models.melody_rnn import melody_rnn_sequence_generator
-# from magenta.models.shared import sequence_generator_bundle
-# from note_seq.protobuf import generator_pb2
-# from note_seq.protobuf import music_pb2
+import magenta
+import note_seq
+import tensorflow
+from magenta.models.melody_rnn import melody_rnn_sequence_generator
+from magenta.models.shared import sequence_generator_bundle
+from note_seq.protobuf import generator_pb2
+from note_seq.protobuf import music_pb2
 import py_midicsv as pm
 
 
-class g(object):
+class g():
     def __init__(self):
         # Initialize the model.
         print("Initializing Melody RNN...")
@@ -35,19 +35,29 @@ class g(object):
         generator_options = generator_pb2.GeneratorOptions()
         generator_options.args['temperature'].float_value = temperature
         generate_section = generator_options.generate_sections.add(
-        start_time=last_end_time + seconds_per_step,
-        end_time=total_seconds)
+            start_time=last_end_time + seconds_per_step,
+            end_time=total_seconds)
 
         # Ask the model to continue the sequence.
         sequence = self.melody_rnn.generate(input_sequence, generator_options)
 
-        generated_notes = [n for n in sequence.notes if n.start_time >= generate_section.start_time]
-        
+        start = 0
+        first = 1
+        generated_notes = []
+        for note in sequence.notes:
+            if generate_section.start_time <= note.start_time < generate_section.end_time:
+                if(first): 
+                    start = note.start_time
+                    first = 0
+                note.start_time-=start
+                note.end_time-=start
+                generated_notes.append(note)
+
         # Create a new sequence with only the generated notes
         generated_section_sequence = music_pb2.NoteSequence()
         generated_section_sequence.notes.extend(generated_notes)
 
-        return generated_notes
+        return generated_section_sequence
 
     def seq_proto_to_csv(self, seq, out_csv_filename): # input is in seq_proto format, returns midi
         note_seq.sequence_proto_to_midi_file(seq, 'output.mid')
@@ -60,7 +70,7 @@ class g(object):
         str1 = ""
         with open(in_csv_filename, "r") as f:
             str1 = f.readline()
-        measures, tempo = self.parse_comment(str1)
+        measures, tempo, temperature = self.parse_comment(str1)
 
         midi_object = pm.csv_to_midi(in_csv_filename) 
         with open("output.mid", "wb") as output_file:
@@ -69,27 +79,39 @@ class g(object):
 
         ns = note_seq.midi_file_to_sequence_proto("output.mid")
         
-        return ns, measures, tempo
+        return ns, measures, tempo, temperature
     
     def parse_comment(self, str1):
-        # remove hashtag
-        str1 = str1[1:]
+        # parses comment of form exactly: '#measures tempo temperature'
+            # remove hashtag
+            str1 = str1[1:]
 
-        # grab until space for num measures
-        m_str = ""
-        for c in str1:
-            if not c == ' ':
-                m_str += c
-                str1 = str1[1:]
-            else: # found a space
-                str1 = str1[1:] 
-                break
-        m_num = int(m_str)
-        
-        # rest is tempo info
-        t_num = int(str1)
+            # grab until space for num measures
+            m_str = ""
+            for c in str1:
+                if not c == ' ':
+                    m_str += c
+                    str1 = str1[1:]
+                else: # found a space
+                    str1 = str1[1:] 
+                    break
+            m_num = int(m_str)
+            
+            # rest is tempo info
+            t_str = ""
+            for c in str1:
+                if not c == ' ':
+                    t_str += c
+                    str1 = str1[1:]
+                else: # found a space
+                    str1 = str1[1:] 
+                    break
+            t_num = int(t_str)
 
-        return m_num, t_num
+            # rest is temperature
+            temp_num = int(str1)
+
+            return m_num, t_num, temp_num
     
 
 
@@ -118,7 +140,7 @@ def main():
     in_csv_filename = "in.csv"
     out_csv_filename = "out.csv"
     m = monitor(in_csv_filename) 
-    g = g()
+    gen = g()
 
     while(True):
         changed = False
@@ -127,11 +149,10 @@ def main():
             changed = m.changedQ()
         
         # generate a new melody and write to out file
-        in_seq, num_measures, tempo = g.csv_to_seq_proto(in_csv_filename)
-        print("measures: {}\t tempo: {}".format(num_measures, tempo))
+        in_seq, num_measures, tempo, temperature = gen.csv_to_seq_proto(in_csv_filename)
         # still not sure how to use num_measures to calc num_steps TO DO add support for this
-        out_seq = g.generate(in_seq, tempo, num_steps=128, temperature=1)
-        g.seq_proto_to_csv(out_seq, out_csv_filename)
+        out_seq = gen.generate(in_seq, tempo, num_steps=128, temperature=temperature)
+        gen.seq_proto_to_csv(out_seq, out_csv_filename)
     
 
 if __name__ == "__main__":
